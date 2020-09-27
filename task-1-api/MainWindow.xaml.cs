5d3c87c1aea876cac2461bd9f0c43913981f3c00
VladislavMaksimov <workmaximov@yandex.ru>
2020-09-27 19:42:11 +0500

    Requests to services generating random content added.

--- task-1-api/MainWindow.xaml.cs
+++ task-1-api/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.Json;
 
 namespace task_1_api
 {
@@ -20,6 +21,28 @@ namespace task_1_api
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void generateRandom_Click(object sender, RoutedEventArgs e)
+        {
+            JsonDocument postText = JsonDocument.Parse(RandomRequester.getText());
+            JsonDocument postImage = JsonDocument.Parse(RandomRequester.getImage());
+
+            string uriImage = postImage.RootElement[0].GetProperty("url").ToString();
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(uriImage, UriKind.Absolute);
+            bitmap.EndInit();
+
+            cat.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            cat.Source = bitmap;
+            messageText.Text = postText.RootElement[0].ToString();
         }
     }
 }
