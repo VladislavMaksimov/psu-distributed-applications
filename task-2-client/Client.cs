1d91c34dd7124fa9e6cda40a1d7937dc27f5b921
VladislavMaksimov <workmaximov@yandex.ru>
2020-11-24 23:00:15 +0500

    Started task-2. Websockets added.

---
+++ task-2-client/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WebSocket = WebSocketSharp.WebSocket;
+
+namespace task_2
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://localhost:8080/"))
+            {
+                ws.OnOpen += (sender, e) => Console.WriteLine("Connected to server.");
+                ws.OnMessage += (sender, e) => Console.WriteLine(e.Data);
+                ws.Connect();
+
+                while (true)
+                {
+                    string msg = Console.ReadLine();
+                    ws.Send(msg);
+                }
+            }
+        }
+    }
+}
---
+++ task-2-server/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace task_2_server
+{
+    class Server
+    {
+        public class ServerBehavior : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Send("You just have sent \"" + e.Data + "\" to me.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://localhost:8080");
+            wssv.AddWebSocketService<ServerBehavior>("/");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
