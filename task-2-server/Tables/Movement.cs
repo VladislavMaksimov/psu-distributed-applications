7a462c59df076eb733b6a8d49fe98ebf9b4cc6d3
VladislavMaksimov <workmaximov@yandex.ru>
2020-11-25 16:35:31 +0500

    Conntection to SQLite and PostgreSQL added.

---
+++ task-2-server/Tables/Movement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_2.Tables
+{
+    class Movement : Directory
+    {
+        public Movement(string name) : base(name) { }
+    }
+}
